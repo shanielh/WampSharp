50d57aae40a98e445d6610f30fe5fd941363927e
Shani Elharrar <atnt123@bezeqint.net>
2013-11-21 16:19:17 +0200

    Reformated the creation of the Serializer method.

--- src/WampSharp/Core/Proxy/WampOutgoingRequestSerializer.cs
+++ src/WampSharp/Core/Proxy/WampOutgoingRequestSerializer.cs
@@ -36,51 +36,6 @@ namespace WampSharp.Core.Proxy
         public WampMessage<TMessage> SerializeRequest(MethodInfo method, object[] arguments)
         {
             return mSerializers.GetOrAdd(method, GetSerializer)(arguments, mFormatter);
-            /*
-            WampHandlerAttribute attribute = mMethodToHandlerAttribute.GetOrAdd(method,
-                m => m.GetCustomAttribute<WampHandlerAttribute>(true));
-
-            WampMessageType messageType = attribute.MessageType;
-
-            WampMessage<TMessage> result = new WampMessage<TMessage>()
-                                               {
-                                                   MessageType = messageType
-                                               };
-
-            var parameters = method.GetParameters()
-                                   .Zip(arguments,
-                                        (parameterInfo, argument) =>
-                                        new
-                                            {
-                                                parameterInfo,
-                                                argument
-                                            })
-                                   .Where(x => !x.parameterInfo.IsDefined(typeof(WampProxyParameterAttribute), true));
-
-            List<TMessage> messageArguments = new List<TMessage>();
-
-            foreach (var parameter in parameters)
-            {
-                if (!parameter.parameterInfo.IsDefined(typeof(ParamArrayAttribute), true))
-                {
-                    TMessage serialized = mFormatter.Serialize(parameter.argument);
-                    messageArguments.Add(serialized);
-                }
-                else
-                {
-                    object[] paramsArray = parameter.argument as object[];
-
-                    foreach (object param in paramsArray)
-                    {
-                        TMessage serialized = mFormatter.Serialize(param);
-                        messageArguments.Add(serialized);
-                    }
-                }
-            }
-
-            result.Arguments = messageArguments.ToArray();
-
-            return result;*/
         }
 
         private static Func<object[], IWampFormatter<TMessage>,  WampMessage<TMessage>> GetSerializer(MethodInfo methodInfo)
@@ -97,29 +52,39 @@ namespace WampSharp.Core.Proxy
                     .Select((p, i) => new { OldIndex = p.Index, NewIndex = i, p.Parameter, p.ParamArray })
                     .ToList();
 
+            // Get the last parameter (Might have a "ParamsAttribute")
             var lastParameter = parameters.Last();
 
+            // Function Arguments
             ParameterExpression objectArrayParameter = Expression.Parameter(typeof (object[]));
             ParameterExpression formatterParameter = Expression.Parameter(typeof (IWampFormatter<TMessage>));
+            
+            // Variables
             ParameterExpression argumentsVariable = Expression.Variable(typeof (TMessage[]));
             ParameterExpression returnValue = Expression.Variable(typeof (WampMessage<TMessage>));
-            IList<Expression> methodBody = new List<Expression>();
             IList<ParameterExpression> variables = new List<ParameterExpression> { argumentsVariable, returnValue };
+
+            // Method Body
+            IList<Expression> methodBody = new List<Expression>();
 
             Expression retValLengthExpression;
 
             ParameterExpression paramArrayValue = Expression.Parameter(lastParameter.Parameter.ParameterType);
             
+            // If the last attribute is a params, we need to 'flatten' it into the returned arguments
             if (lastParameter.ParamArray)
             {
+                // Add a object[] variable
                 variables.Add(paramArrayValue);
 
+                // And set the value
                 methodBody.Add(
                     Expression.Assign(paramArrayValue,
                         Expression.Convert(
                             Expression.ArrayIndex(objectArrayParameter, Expression.Constant(lastParameter.OldIndex)),
                             paramArrayValue.Type)));
 
+                // Setting the returned arguments length
                 retValLengthExpression = Expression.Add(Expression.Constant(parameters.Count - 1),
                     Expression.ArrayLength(paramArrayValue));
             }
@@ -128,43 +93,55 @@ namespace WampSharp.Core.Proxy
                 retValLengthExpression = Expression.Constant(parameters.Count);
             }
 
+            // Creating new array for returned arguments
             methodBody.Add(Expression.Assign(argumentsVariable, Expression.NewArrayBounds(typeof(TMessage), retValLengthExpression)));
 
+            // Serializing all the values except the param arrays
             foreach (var p in parameters.Where(p => !p.ParamArray))
             {
                 methodBody.Add(Expression.Assign(Expression.ArrayAccess(argumentsVariable, Expression.Constant(p.NewIndex)),
                     Expression.Call(formatterParameter, "Serialize", null, Expression.ArrayIndex(objectArrayParameter, Expression.Constant(p.OldIndex)))));
             }
 
+            // Flatting the param array value to the returned arguments
             if (lastParameter.ParamArray)
             {
+                // Creating i and count variables
                 ParameterExpression i = Expression.Parameter(typeof (int), "i");
                 ParameterExpression count = Expression.Parameter(typeof(int), "count");
+                
                 variables.Add(i);
                 variables.Add(count);
 
+                // i = 0, count = lastParameter.Length
                 methodBody.Add(Expression.Assign(i, Expression.Constant(0)));
                 methodBody.Add(Expression.Assign(count, Expression.ArrayLength(paramArrayValue)));
 
                 LabelTarget breakLabel = Expression.Label();
                 LabelTarget continueLabel = Expression.Label();
 
+                // Adding loop
                 methodBody.Add(Expression.Loop(Expression.Block(
-                    Expression.IfThen(Expression.MakeBinary(ExpressionType.GreaterThanOrEqual, i, count), Expression.Goto(breakLabel)),
-                    Expression.Assign(Expression.ArrayAccess(argumentsVariable, Expression.Add(Expression.Constant(lastParameter.NewIndex), i)), Expression.Call(formatterParameter, "Serialize", null, Expression.ArrayIndex(paramArrayValue, i))),
+                    Expression.IfThen(Expression.MakeBinary(ExpressionType.GreaterThanOrEqual, i, count),
+                        Expression.Goto(breakLabel)),
+                    Expression.Assign(
+                        Expression.ArrayAccess(argumentsVariable,
+                            Expression.Add(Expression.Constant(lastParameter.NewIndex), i)),
+                        Expression.Call(formatterParameter, "Serialize", null, Expression.ArrayIndex(paramArrayValue, i))),
                     Expression.PostIncrementAssign(i),
                     Expression.Goto(continueLabel)
                     ), breakLabel, continueLabel));
-
             }
 
-
+            // Creating the return value
             methodBody.Add(Expression.Assign(returnValue, Expression.New(typeof(WampMessage<TMessage>))));
             methodBody.Add(Expression.Assign(Expression.Property(returnValue, "MessageType"), Expression.Constant(type)));
             methodBody.Add(Expression.Assign(Expression.Property(returnValue, "Arguments"), argumentsVariable));
 
+            // Returning the value (Last expression in method body is the returned value, same as ruby)
             methodBody.Add(returnValue);
 
+            // Compiling the method
             var lambdaExpression = Expression.Lambda<Func<object[], IWampFormatter<TMessage>, WampMessage<TMessage>>>(
                 Expression.Block(variables, methodBody), objectArrayParameter, formatterParameter);
 
